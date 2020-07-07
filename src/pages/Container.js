7e9a52807695c0c605ae9db47b4ef27e76fef5fe
KevinSizz <kevinskate.kg@gmail.com>
2020-07-06 19:39:44 -0500

    bgColor

--- src/pages/Container.js
+++ src/pages/Container.js
@@ -3,41 +3,45 @@ import '../styles/Container.css'
 import Photo from '../util/images/photoProfile.jpeg'
 import Proyects from './Proyects'
 import { Link } from 'react-router-dom';
+//import '../styles/BackGround/css/style.css'
 
 function Container() {
     const [proyectType, setProyectType]= useState('movil')
     return (
-        <div className="Container">
-            <div className="Header">
-                <h1>Kevin Guzmán</h1>
-                <img className="Header_Photo" src={Photo}/>
-                <div className="Header_Link">
-                    <button 
-                        className="btn" 
-                        onClick={()=>setProyectType('educacion')} 
-                    >
-                        <a style={Styles.textButton}>Educacion</a>
-                    </button>
-                    <button 
-                        className="btn" 
-                        onClick={()=>setProyectType('movil')} 
-                    >
-                        <a style={Styles.textButton}>Desarrollo Movil</a>
-                    </button>
-                    <button 
-                        className="btn" 
-                        onClick={()=>setProyectType('hardware')} 
-                    >
-                        <a style={Styles.textButton}>Hardware</a>
-                    </button>
+        
+            <div className="Container"  >
+                <div className="Header"   >
+                    <h1>Kevin Guzmán</h1>
+                    <img className="Header_Photo" src={Photo}/>
+                    <div className="Header_Link">
+                        <button 
+                            className="btn" 
+                            onClick={()=>setProyectType('educacion')} 
+                        >
+                            <a style={Styles.textButton}>Educacion</a>
+                        </button>
+                        <button 
+                            className="btn" 
+                            onClick={()=>setProyectType('movil')} 
+                        >
+                            <a style={Styles.textButton}>Desarrollo Movil</a>
+                        </button>
+                        <button 
+                            className="btn" 
+                            onClick={()=>setProyectType('hardware')} 
+                        >
+                            <a style={Styles.textButton}>Hardware</a>
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className="Body">
-                <Proyects
-                    proyectType={proyectType}
-                />
-            </div>
-        </div>
+                <div className="Body"  >
+                    <Proyects
+                        proyectType={proyectType}
+                    />
+                </div>
+            </div>   
+        
+        
     )
 }
 
